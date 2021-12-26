262e8875e3ab01c8d3e748341be05fc6d3d56ac4
hailelagi <52631736+hailelagi@users.noreply.github.com>
2021-12-26 01:00:48 +0100

    add: rust(day two part two)

--- 2021/rust/day-2-dive/src/main.rs
+++ 2021/rust/day-2-dive/src/main.rs
@@ -8,7 +8,7 @@ struct Command {
 
 fn main() {
     println!("{:?}", course_path("./puzzle_input.txt"));
-    // println!("{:?}", corrected_course_path("./puzzle_input.txt"));
+    println!("{:?}", corrected_course_path("./puzzle_input.txt"));
 }
 
 // Part One: Calculates the horizontal position and depth of the course
@@ -22,7 +22,7 @@ fn course_path(path: &str) -> u32 {
             "forward" => horizontal += command.position,
             "down" => depth += command.position,
             "up" => depth -= command.position,
-            _ => ()
+            _ => (),
         }
     }
 
@@ -30,10 +30,25 @@ fn course_path(path: &str) -> u32 {
 }
 
 // Part Two: Calculates the horizontal position and depth, using new interpretation of the commands
-fn corrected_course_path(path: &str) -> i32 {
-    let _commands = parse(path);
+fn corrected_course_path(path: &str) -> u32 {
+    let commands = parse(path);
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
 
-    return 0;
+    for command in commands {
+        match command.direction.as_str() {
+            "forward" => {
+                horizontal += command.position;
+                depth += aim * command.position
+            },
+            "down" => aim += command.position,
+            "up" => aim -= command.position,
+            _ => ()
+        }
+    }
+
+    return horizontal * depth;
 }
 
 fn parse(path: &str) -> Vec<Command> {
@@ -43,13 +58,16 @@ fn parse(path: &str) -> Vec<Command> {
     let mut commands: Vec<Command> = Vec::new();
 
     for input in puzzle_input {
-        let command: Vec<&str>  = input.split(" ").collect();
+        let command: Vec<&str> = input.split(" ").collect();
         let direction = command[0].to_string();
         let position = command[1].parse().unwrap();
 
-        commands.push(Command{direction, position})
+        commands.push(Command {
+            direction,
+            position,
+        })
     }
-    
+
     return commands;
 }
 
@@ -62,9 +80,8 @@ mod tests {
         assert_eq!(course_path("./example_input.txt"), 150);
     }
 
-    /*
     #[test]
     fn test_corrected_course_path() {
         assert_eq!(corrected_course_path("./example_input.txt"), 900);
-    } */
+    }
 }
