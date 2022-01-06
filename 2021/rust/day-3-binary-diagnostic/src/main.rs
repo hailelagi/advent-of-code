f41304f1c68865c0e8f93f872eb232fb966c6afb
hailelagi <52631736+hailelagi@users.noreply.github.com>
2022-01-06 05:44:15 +0100

    add: rust(day 3) part two

--- 2021/rust/day-3-binary-diagnostic/src/main.rs
+++ 2021/rust/day-3-binary-diagnostic/src/main.rs
@@ -3,15 +3,16 @@ use std::fs;
 
 fn main() {
     println!("{:?}", power_consumption("./puzzle_input.txt"));
-    // println!("{:?}", life_support_rating("./puzzle_input.txt"));
+    println!("{:?}", life_support_rating("./puzzle_input.txt"));
 }
 
 // Part one: Find the power consumption of a submarine
 pub fn power_consumption(path: &str) -> u32 {
     let diagnostic_report = parse(path);
-    let mut positions = HashMap::<u32, u32>::new();
     let limit = diagnostic_report[0].len();
     let report_length = diagnostic_report.len();
+
+    let mut positions = HashMap::<u32, u32>::new();
 
     for report in diagnostic_report {
         for (i, c) in report.chars().enumerate() {
@@ -19,7 +20,7 @@ pub fn power_consumption(path: &str) -> u32 {
                 *positions.entry(i.try_into().unwrap()).or_insert(0) += 1;
             }
         }
-    };
+    }
 
     let mut gamma_rate = String::new();
     let mut epsilon_rate = String::new();
@@ -41,13 +42,50 @@ pub fn power_consumption(path: &str) -> u32 {
 }
 
 // Part two: Find the life support rating
+// yes yes this is a terrible terrible solution. Believe me I know.
+// I have been stuck on this for several days. I suck so bad @ rust.
+//  ----------------/// -----------
+//  * I tried scan, reduce(rust has fold) no luck.
+//  * I tried mutating the Vector while iterating. The compiler got mad.
+//    reasonable, but annoying nonetheless.
+//  * When I have a better grasp on rust perhaps I can better express this problem
+//   in some idomatic manner rather than copy pasta my elixir solution with bad modifications.
+//  * I know, recursion on unbounded inputs is a terrible idea in rust... I just don't know what to do.
 pub fn life_support_rating(path: &str) -> u32 {
     let diagnostic_report = parse(path);
-    let oxygen_generator_rating = 0;
-    let co2_scrubber_rating = 0;
 
-    // println!("{:?}", diagnostic_report);
-    return oxygen_generator_rating * co2_scrubber_rating
+    let oxygen_generator_rating = find_rating(0, diagnostic_report.clone(), "oxygen");
+    let co2_scrubber_rating = find_rating(0, diagnostic_report.clone(), "co2");
+
+    return to_decimal(&oxygen_generator_rating) * to_decimal(&co2_scrubber_rating)
+}
+
+fn find_rating(index: usize, diagnostic_report: Vec<String>, rating_type: &str) -> String {
+    let mut one_count = 0;
+    let mut zero_count = 0;
+    let mut bit = "0";
+
+    for report in &diagnostic_report {
+        if report.chars().nth(index).unwrap().to_string() == "1" {
+            one_count += 1;
+        } else {
+            zero_count += 1;
+        }
+    }
+
+    if one_count >= zero_count && rating_type == "oxygen" { bit = "1"; }
+    if one_count < zero_count && rating_type == "co2" { bit = "1"; }
+
+    let new_report: Vec<String> = diagnostic_report
+        .into_iter()
+        .filter(|report| report.chars().nth(index).unwrap().to_string() == bit)
+        .collect();
+
+    if new_report.len() == 1 {
+        return new_report[0].clone();
+    } else {
+        find_rating(index + 1, new_report, rating_type)
+    }
 }
 
 fn to_decimal(binary: &str) -> u32 {
