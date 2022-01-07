d81f3df57cce4bf2a25eab6d77325e937a5828d5
hailelagi <52631736+hailelagi@users.noreply.github.com>
2022-01-07 23:22:12 +0100

    add: wip elixir day 4 part one

--- 2021/elixir/day-4-giant-squid/giant_squid_test.exs
+++ 2021/elixir/day-4-giant-squid/giant_squid_test.exs
@@ -3,15 +3,15 @@ ExUnit.start()
 defmodule GiantSquidTest do
   use ExUnit.Case, async: true
 
-  describe "" do
-    test "" do
-      assert GiantSquid.("./example_input.txt") == nil
+  describe "Calculate which board will win first and the final score if chosen." do
+    test "the example game is scored properly" do
+      assert GiantSquid.wining_board_score("./example_input.txt") == 4512
     end
   end
 
   describe "" do
     test "" do
-      assert GiantSquid.("./example_input.txt") == nil
+      # assert GiantSquid.("./example_input.txt") == nil
     end
   end
 end
