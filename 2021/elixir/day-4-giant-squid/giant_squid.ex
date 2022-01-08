2a3a97f7484092a3708dd8b2a2357c0f1d0abea4
hailelagi <52631736+hailelagi@users.noreply.github.com>
2022-01-08 07:37:25 +0100

    add: elixir day 4 part one; very buggy idk :/

--- 2021/elixir/day-4-giant-squid/giant_squid.ex
+++ 2021/elixir/day-4-giant-squid/giant_squid.ex
@@ -2,65 +2,81 @@ defmodule GiantSquid do
   @doc """
   Part one: Calculate which board will win first and the final score if chosen.
   """
-
-  # Approach one brute force: 
-  # sum the rows and columns - precomputed comparision will run at most O(m * n) where:
-  # m is number of elements in the row and n the column.
-  # iterate through numbers and find all unique combinations of 5 O(n!) compare with sum
-  # O(m * n) + O(n!) doesn't seem terribly efficient. 
-
-  # Approach two after a cup of coffee :)
-  # iterate over random numbers
-  # it is guaranteed a bingo board is a 5x5 matrix.
-  # store the postions found in each board as a map of their index [0 - 24]
-  # possible impl optimisiation use a tuple to store boards only need read access
-  # hard code the "match" e.g if i 0 - 4 is "not nil" we have a match
-  # a bit verbose but seems more efficient
-  # check each board on each iteration for a winner
-  # fiter by index.
-  # trade time for space and less flexibility
-  # worst case O(n ^ boards)
-
   def wining_board_score(path) do
     {numbers, boards} = parse(path)
-    num_boards = length(boards)
-    num_len = length(numbers)
+    board_len = length(boards) - 1
 
-      # fiter by index.
-    IO.inspect search(numbers, boards)
+    scores = for i <- 0..board_len, into: %{}, do: {i, []}
 
+    winning_board = search(board_len, numbers, boards, scores)
+    [called_index, {board_no, marked_numbers }] = winning_board
+
+    last_called = Enum.at(numbers, called_index) |> String.to_integer()
+
+    sum_unmarked =  Enum.at(boards, board_no)
+    |> Enum.with_index(fn elem, index-> if index in marked_numbers, do: nil, else: String.to_integer(elem) end )
+    |> Enum.filter(fn n -> n != nil end)
+    |> Enum.sum()
+
+    last_called * sum_unmarked
   end
 
-  defp search(index \\ 0, numbers, boards, scores \\ %{})
+  defp search(index \\ 0, board_len, numbers, boards, scores)
 
   # win not possible
-  defp search(_, [], _, scores), do: scores
+  defp search(_, _, [], _, scores), do: scores
 
-  defp search(index, numbers, boards, scores) do
+  defp search(index, board_len, numbers, boards, scores) do
     [n | tail] = numbers
-    board_len = length(boards) - 1
 
     positions = Enum.map(boards, fn board -> 
       Enum.find_index(board, fn b -> b == n end) 
     end)
-    
-    score = for i <- 0..board_len do %{i => Enum.at(positions, i)} end
 
-    IO.inspect score
-    #scores = Map.put(scores, n, score)
-
-    search(index =+ 1, tail, boards, scores)
+    score = for i <- 0..board_len, into: %{}, do: {i, [Enum.at(positions, i)]}
+    scores = Map.merge(scores, score, fn _, x, y -> 
+      Enum.concat(x, y)
+    end) 
+    case wining_match(scores) do
+      {:ok, win_board} -> [index | win_board]
+      {:continue, _} -> 
+        index = index + 1
+        search(index, board_len, tail, boards, scores)
+    end
   end
 
-  defp wining_match() do 
-      #  hard code the "match" e.g if i 0 - 4 is "not nil" we have a match
-      # 0, 1, 2, 3, 4
-      # 5, 6, 7, 8, 9
-      # 10,11,12,13,14
-      # 15,16,17,18,19
-      # 20,21,22,23,24
+  defp wining_match(scores) do 
+      if length(scores[0]) < 5 do
+        {:continue, nil}
+      else
+        res = Enum.map(scores, fn {board_number, score} -> 
+          window = score
+          |> Enum.sort(:asc) 
+          |> Enum.chunk_every(5, 5,:discard)
+          |> Enum.map(fn interval -> 
+            [incr | score] = interval
 
+            Enum.reduce_while(score, incr, fn x, acc ->
+              if (x - acc == 1 or x - acc == 5) and (incr in 0..4) do
+                {:cont, x }
+                  else
+                {:halt, -1}
+              end
+            end)
+          end)
+
+          if Enum.any?(window, fn x -> x != -1 end), do: {board_number, score}, else: 0
+        end)
+        |> Enum.filter(fn r -> r != 0 end)
+
+      if not Enum.empty?(res) do
+        {:ok, res}
+        else
+        {:continue, nil}
+      end
+      end
   end
+
 
   @doc """
   Part two:
@@ -80,4 +96,4 @@ defmodule GiantSquid do
   end
 end
 
-GiantSquid.wining_board_score("./example_input.txt")
+IO.puts GiantSquid.wining_board_score("./puzzle_input.txt")
