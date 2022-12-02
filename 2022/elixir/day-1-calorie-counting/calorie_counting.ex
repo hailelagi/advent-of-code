d12d5cdd0fea566bffab14171cac80fd7c3f21f4
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-02 02:06:31 +0100

    feat: calorie counting part one

--- 2022/elixir/day-1-calorie-counting/calorie_counting.ex
+++ 2022/elixir/day-1-calorie-counting/calorie_counting.ex
@@ -7,14 +7,18 @@ defmodule CalorieCounting do
   def elf_with_most(path \\ "./puzzle_input.txt") do
     calories = parse(path)
 
-    0
+    calories
+    |> Enum.map(fn c ->  Enum.sum(c) end)
+    |> Enum.max()
   end
 
   defp parse(path) do
     calories = File.read!(path)
 
     calories
-    |> String.split("\n")
-    # |> Enum.map(&String.to_integer/1)
+    |> String.split("\n\n")
+    |> Stream.map(fn inventory ->
+      inventory |> String.split("\n") |> Enum.map(&String.to_integer/1)
+    end)
   end
 end
