1770e3d19144da5208529e89691434501aa62282
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-08 01:35:37 +0100

    feat(elixir): day 2 part one

--- 2022/elixir/day-2-rock-paper-scissors/rock_paper_scissors.ex
+++ 2022/elixir/day-2-rock-paper-scissors/rock_paper_scissors.ex
@@ -1,16 +1,56 @@
 defmodule RockPaperScissors do
-  @moduledoc false
+  @moduledoc """
+    Finds the outcome of a game of rock paper scissors between elves.
+    lose = 0, draw = 3 and win = 6 points
+    rock(1) -> A
+    paper(2) -> B
+    scissors(3) -> C
+  """
+  alias Mix.Tasks.Xref
+  alias Mix.Tasks.Compile.Yecc
+  alias JasonVendored.Encode
 
+  @doc """
+    It gets the total score by following the encrypted strategy guide
+    X for Rock, Y for Paper, and Z for Scissors
+  """
   def total_score(path \\ "./puzzle_input.txt") do
     strategy = parse(path)
-    IO.inspect(strategy)
 
-    0
+    strategy
+    |> Enum.map(fn s -> outcomes(s) end)
+    |> Enum.map(fn o -> find_score(o) end)
+    |> Enum.sum()
+  end
+
+  defp find_score({outcome, point}) do
+    case outcome do
+      :win -> 6 + point
+      :draw -> 3 + point
+      :lose -> 0 + point
+    end
+  end
+
+  def outcomes(round) do
+    case round do
+      ["A", "Y"] -> {:win, 2}
+      ["B", "Z"] -> {:win, 3}
+      ["C", "X"] -> {:win, 1}
+      ["A", "X"] -> {:draw, 1}
+      ["B", "Y"] -> {:draw, 2}
+      ["C", "Z"] -> {:draw, 3}
+      [_, "X"] -> {:lose, 1}
+      [_, "Y"] -> {:lose, 2}
+      [_, "Z"] -> {:lose, 3}
+    end
   end
 
   defp parse(path) do
     strategy_guide = File.read!(path)
 
     String.split(strategy_guide, "\n")
+    |> Enum.map(&String.split(&1, " "))
   end
 end
+
+IO.inspect(RockPaperScissors.total_score())
--- 2022/elixir/day-2-rock-paper-scissors/rock_paper_scissors_test.exs
+++ 2022/elixir/day-2-rock-paper-scissors/rock_paper_scissors_test.exs
@@ -3,7 +3,7 @@ ExUnit.start()
 defmodule RockPaperScissorsTest do
   use ExUnit.Case, async: true
 
-  test "do something" do
+  test "it gets the score if following the strategy guide" do
     assert RockPaperScissors.total_score("./example_input.txt") == 15
   end
 end
