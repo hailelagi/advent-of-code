7fe862d1e382a6b174b6c0753bfe67a542c47593
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-09 14:33:44 +0100

    feat(elixir): day 4 part one

--- 2022/elixir/day-4-camp-cleanup/camp_cleanup.ex
+++ 2022/elixir/day-4-camp-cleanup/camp_cleanup.ex
@@ -1,16 +1,31 @@
 defmodule CampCleanup do
+  @moduledoc false
 
   def pairs_contain_count(path \\ "./puzzle_input.txt") do
     assignments = parse(path)
 
-    IO.inspect(assignments)
-    0
+    Enum.count(assignments, fn [first, second] ->
+      first = set_range(first)
+      second = set_range(second)
+
+      MapSet.subset?(first, second) or MapSet.subset?(second, first)
+    end)
   end
 
+  defp set_range(section) do
+    [upper, lower] = String.split(section, "-") |> Enum.map(&String.to_integer/1)
+
+    Range.new(upper, lower) |> MapSet.new()
+  end
 
   defp parse(path) do
     section_assignments = File.read!(path)
 
-    String.split(section_assignments, "\n")
+    section_assignments
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, ","))
   end
 end
+
+
+IO.inspect(CampCleanup.pairs_contain_count())
