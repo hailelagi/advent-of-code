1d356b4e17de60a6446f56a28382c1181ccd1573
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-09 14:42:02 +0100

    feat(elixir): day 4 part two

--- 2022/elixir/day-4-camp-cleanup/camp_cleanup.ex
+++ 2022/elixir/day-4-camp-cleanup/camp_cleanup.ex
@@ -1,6 +1,9 @@
 defmodule CampCleanup do
   @moduledoc false
 
+  @doc """
+    Part one: count how many assignment pairs does one range fully contain the other
+  """
   def pairs_contain_count(path \\ "./puzzle_input.txt") do
     assignments = parse(path)
 
@@ -9,6 +12,20 @@ defmodule CampCleanup do
       second = set_range(second)
 
       MapSet.subset?(first, second) or MapSet.subset?(second, first)
+    end)
+  end
+
+  @doc """
+    Part two: count assignment pairs do the ranges overlap
+  """
+  def overlap(path \\ "./puzzle_input.txt") do
+    assignments = parse(path)
+
+    Enum.count(assignments, fn [first, second] ->
+      first = set_range(first)
+      second = set_range(second)
+
+      MapSet.intersection(first, second) |> Enum.empty?() == false
     end)
   end
 
@@ -27,5 +44,5 @@ defmodule CampCleanup do
   end
 end
 
-
 IO.inspect(CampCleanup.pairs_contain_count())
+IO.inspect(CampCleanup.overlap())
