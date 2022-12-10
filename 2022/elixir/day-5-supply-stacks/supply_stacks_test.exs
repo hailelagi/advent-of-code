b422b7a543e31017044a59e79ef9970ba229fba4
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-10 01:42:12 +0100

    feat(elixir): part one day 5

--- 2022/elixir/day-5-supply-stacks/supply_stacks_test.exs
+++ 2022/elixir/day-5-supply-stacks/supply_stacks_test.exs
@@ -1,28 +1,57 @@
 defmodule SupplyStacks do
-  @moduledoc """
-   elixir -r "supply_stacks.ex" supply_stacks_test.exs
+  @moduledoc false
+
+  @doc """
+    Part one: crates at the top after arrangement
   """
-
   def top_crate(path \\ "./puzzle_input.txt") do
-    [diagram, _procedure] = parse(path)
-    stack = build_stack(diagram)
+    [diagram, procedures] = parse(path)
 
-    IO.inspect(stack)
-    "FAIL"
+    stacks =
+      for {s, i} <- build_stack(diagram), into: %{} do
+        {i, Enum.filter(Tuple.to_list(s), &(&1 != ""))}
+      end
+
+    procedures = String.split(procedures, "\n")
+
+    rearranged = Enum.reduce(procedures, stacks, fn p, acc -> rearrangement(acc, p) end)
+
+    rearranged
+    |> Enum.into([])
+    |> Enum.map(fn {_, stack} -> List.last(stack) end)
+    |> Enum.join()
+    |> String.split(~r/[[:punct:]]/)
+    |> Enum.join()
   end
 
-  # so inefficient but oh well, it works!
+  defp rearrangement(stacks, procedure) do
+    ["move", m, "from", f, "to", t] = String.split(procedure, " ")
+    [move, from, to] = Enum.map([m, f, t], &String.to_integer/1)
+
+    from_index = from - 1
+    to_index = to - 1
+
+    {taken, new_list} = take_from(stacks[from_index], move)
+    prev = stacks[to_index]
+
+    %{stacks | from_index => new_list, to_index => prev ++ taken}
+  end
+
+  def take_from(list, n) do
+    {Enum.take(list, -n) |> Enum.reverse(), Enum.drop(list, -n)}
+  end
+
   defp build_stack(diagram) do
-    [numbers | crates] = String.split(diagram, "\n") |> Enum.reverse()
-    len = String.strip(numbers) |> String.split("  ") |> length()
+    [_numbers | crates] = String.split(diagram, "\n") |> Enum.reverse()
 
     crates
-    |> Enum.map(&String.split(&1, " "))
-    |> Enum.map(&dedup_interval(&1))
-    |> Enum.zip()
+    |> Stream.map(&String.split(&1, " "))
+    |> Stream.map(&dedup_interval(&1))
+    |> Stream.zip()
+    |> Enum.with_index()
   end
 
-  defp dedup_interval(data, interval \\ 4) do
+  defp dedup_interval(data) do
     {_, row} =
       Enum.reduce(data, {0, []}, fn c, {count, rest} ->
         cond do
@@ -42,3 +71,5 @@ defmodule SupplyStacks do
     String.split(crate_drawing, "\n\n")
   end
 end
+
+IO.inspect(SupplyStacks.top_crate())
