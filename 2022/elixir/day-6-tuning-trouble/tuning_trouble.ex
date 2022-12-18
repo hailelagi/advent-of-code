b876f44f9b0ab2474f7bbdc234b2075c888da81c
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-18 23:14:03 +0100

    feat(elixir): day six part one

--- 2022/elixir/day-6-tuning-trouble/tuning_trouble.ex
+++ 2022/elixir/day-6-tuning-trouble/tuning_trouble.ex
@@ -1,7 +1,33 @@
 defmodule TuningTrouble do
   @moduledoc false
 
-  def test do
-    :ok
+  def main do
+    datastream = File.read!("./puzzle_input.txt")
+
+    IO.puts(packet_marker(datastream))
+  end
+
+  @doc """
+    Part one:
+    Find the start of packet marker in a datastream buffer
+  """
+  def packet_marker(datastream) do
+    datastream
+    |> String.splitter("")
+    |> Stream.filter(&(&1 != ""))
+    |> Stream.with_index()
+    |> Stream.chunk_every(4, 1, :discard)
+    |> Enum.reduce_while([], fn x, acc ->
+      packet = Enum.map(x, fn {value, _index} -> value end)
+
+      if length(Enum.uniq(packet)) == length(packet) do
+        [{_value, index}| _] = Enum.reverse(x)
+        {:halt, index + 1}
+      else
+        {:cont, x}
+      end
+    end)
   end
 end
+
+TuningTrouble.main()
--- 2022/elixir/day-6-tuning-trouble/tuning_trouble_test.exs
+++ 2022/elixir/day-6-tuning-trouble/tuning_trouble_test.exs
@@ -3,8 +3,25 @@ ExUnit.start()
 defmodule TuningTroubleTest do
   use ExUnit.Case, async: true
 
-  test "it fails" do
-    # mjqjpqmgbljsphdztnvjfqwrcgsmlb
-    assert true == false
+  describe "detects a start-of-packet marker in the datastream" do
+    test "it detects the example datastream buffer" do
+      assert TuningTrouble.packet_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
+    end
+
+    test "example two" do
+      assert TuningTrouble.packet_marker("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+    end
+
+    test "example three" do
+      assert TuningTrouble.packet_marker("nppdvjthqldpwncqszvftbrmjlhg") == 6
+    end
+
+    test "example four" do
+      assert TuningTrouble.packet_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+    end
+
+    test "example five" do
+      assert TuningTrouble.packet_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+    end
   end
 end
