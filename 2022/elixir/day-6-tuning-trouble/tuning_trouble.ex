7bb1475d6716137d5da3cacc6653c082e1c6f6db
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-18 23:24:45 +0100

    feat(elixir): day 6 part two

--- 2022/elixir/day-6-tuning-trouble/tuning_trouble.ex
+++ 2022/elixir/day-6-tuning-trouble/tuning_trouble.ex
@@ -4,24 +4,32 @@ defmodule TuningTrouble do
   def main do
     datastream = File.read!("./puzzle_input.txt")
 
-    IO.puts(packet_marker(datastream))
+    IO.puts(start_of_marker(datastream, :packet))
+    IO.puts(start_of_marker(datastream, :message))
   end
 
   @doc """
     Part one:
     Find the start of packet marker in a datastream buffer
   """
-  def packet_marker(datastream) do
+  def start_of_marker(datastream, type \\ :packet) do
+    chunks =
+      case type do
+        :packet -> 4
+        :message -> 14
+        _ -> raise "Invalid communication system provided :( "
+      end
+
     datastream
     |> String.splitter("")
     |> Stream.filter(&(&1 != ""))
     |> Stream.with_index()
-    |> Stream.chunk_every(4, 1, :discard)
+    |> Stream.chunk_every(chunks, 1, :discard)
     |> Enum.reduce_while([], fn x, acc ->
       packet = Enum.map(x, fn {value, _index} -> value end)
 
       if length(Enum.uniq(packet)) == length(packet) do
-        [{_value, index}| _] = Enum.reverse(x)
+        [{_value, index} | _] = Enum.reverse(x)
         {:halt, index + 1}
       else
         {:cont, x}
--- 2022/elixir/day-6-tuning-trouble/tuning_trouble_test.exs
+++ 2022/elixir/day-6-tuning-trouble/tuning_trouble_test.exs
@@ -5,23 +5,45 @@ defmodule TuningTroubleTest do
 
   describe "detects a start-of-packet marker in the datastream" do
     test "it detects the example datastream buffer" do
-      assert TuningTrouble.packet_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
+      assert TuningTrouble.start_of_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", :packet) == 7
     end
 
     test "example two" do
-      assert TuningTrouble.packet_marker("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+      assert TuningTrouble.start_of_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", :packet) == 5
     end
 
     test "example three" do
-      assert TuningTrouble.packet_marker("nppdvjthqldpwncqszvftbrmjlhg") == 6
+      assert TuningTrouble.start_of_marker("nppdvjthqldpwncqszvftbrmjlhg", :packet) == 6
     end
 
     test "example four" do
-      assert TuningTrouble.packet_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+      assert TuningTrouble.start_of_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", :packet) == 10
     end
 
     test "example five" do
-      assert TuningTrouble.packet_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+      assert TuningTrouble.start_of_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", :packet) == 11
+    end
+  end
+
+  describe "detects a start-of-message marker in the datastream" do
+    test "it detects the example datastream buffer" do
+      assert TuningTrouble.start_of_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", :message) == 19
+    end
+
+    test "example two" do
+      assert TuningTrouble.start_of_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", :message) == 23
+    end
+
+    test "example three" do
+      assert TuningTrouble.start_of_marker("nppdvjthqldpwncqszvftbrmjlhg", :message) == 23
+    end
+
+    test "example four" do
+      assert TuningTrouble.start_of_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", :message) == 29
+    end
+
+    test "example five" do
+      assert TuningTrouble.start_of_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", :message) == 26
     end
   end
 end
