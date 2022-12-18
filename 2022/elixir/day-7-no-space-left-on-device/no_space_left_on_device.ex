88080e3f10bdaa0619fd45567fc15463a23de877
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-18 23:38:50 +0100

    feat(elixir): day 7 boilerplate

---
+++ 2022/elixir/day-7-no-space-left-on-device/no_space_left_on_device.ex
@@ -0,0 +1,9 @@
+ExUnit.start()
+
+defmodule NoSpaceLeftOnDeviceTest do
+  use ExUnit.Case, async: true
+
+  test "it finds the sum of the total sizes of directories" do
+    assert NoSpaceLeftOnDevice.sum_directories("./example_input.txt") == 95437
+  end
+end
