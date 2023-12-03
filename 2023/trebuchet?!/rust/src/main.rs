ab8a9cbd2bfda68a9ae2dd6913f84b9666987244
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-12-03 11:59:35 +0100

    feat(rust): day 1 part one

--- 2023/trebuchet?!/rust/src/main.rs
+++ 2023/trebuchet?!/rust/src/main.rs
@@ -6,15 +6,35 @@ fn main() {
 
 pub fn sum_calibration_values(path: &str) -> i32 {
     let calibrations = parse(path);
+    let mut sum = 0;
 
-    println!("{:?}", calibrations);
-    14
+    for calibration in calibrations {
+        let result = first_or_last(calibration).parse::<i32>();
+
+        match result {
+            Ok(calibration_value) => sum += calibration_value,
+            Err(_) => ()
+        }
+    }
+
+    sum
 }
 
+fn first_or_last(calibration: String) -> String {
+    let mut digits = calibration.chars().filter(|c| c.to_digit(10).is_some());
+    let result = (digits.next(), digits.last());
+
+    match result {
+        (Some(first), Some(last)) => format!("{}{}", first, last),
+        (Some(first), None) => format!("{}{}", first, first),
+        (None, Some(last)) => format!("{}{}", last, last),
+        (None, None) => format!("{}{}", 0, 0)
+    }
+}
 
 fn parse(path: &str) -> Vec<String> {
     let puzzle_input = fs::read_to_string(path).expect("err reading puzzle input!");
-    let puzzle_input = puzzle_input.split("\n\n").map(|i| i.to_owned()).collect();
+    let puzzle_input = puzzle_input.split("\n").map(|c| c.to_owned()).collect();
 
     return puzzle_input;
 }
