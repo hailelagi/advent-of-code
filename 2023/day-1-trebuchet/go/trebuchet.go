16d5a3a4ea29c60565cc025c28d7efc30e4ed9af
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-12-09 12:46:10 +0100

    feat(go): 2023 day one part one

--- 2023/day-1-trebuchet/go/trebuchet.go
+++ 2023/day-1-trebuchet/go/trebuchet.go
@@ -6,23 +6,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	// Part One
-	fmt.Println(("../puzzle_input.txt"))
+	fmt.Println(SumCalibrations("../puzzle_input.txt"))
+
+	// Part Two
+	fmt.Println(SumCalibrationLetters("../puzzle_input.txt"))
 }
 
-func SumCalibrations(path string) int32 {
-	// calibrations := parsePuzzleInput(path)
+func SumCalibrations(path string) int64 {
+	var sum int64
 
-	// for _, calibration := range calibrations {
-	// 	// for _, _ := range calibration {
-	// 	// 	// fmt.Println(char)
-	// 	// }
-	// }
+	calibrations := parsePuzzleInput(path)
 
-	return 0
+	for _, calibration := range calibrations {
+		var first string
+		var last string
+		var calibrationLength = len(calibration)
+
+		for i := 0; i < calibrationLength; i++ {
+			character := calibration[i : i+1]
+
+			if _, err := strconv.Atoi(character); err == nil {
+				first = character
+				break
+			}
+		}
+
+		for i := calibrationLength; i >= 0; i-- {
+			character := calibration[i-1 : i]
+
+			if _, err := strconv.Atoi(character); err == nil {
+				last = character
+				break
+			}
+		}
+		if value, err := strconv.ParseInt(first+last, 10, 0); err == nil {
+			sum += value
+		}
+	}
+
+	return sum
+}
+
+func SumCalibrationLetters(path string) int64 {
+	var sum int64
+
+	return sum
 }
 
 func parsePuzzleInput(path string) []string {
--- 2023/day-1-trebuchet/go/trebuchet_test.go
+++ 2023/day-1-trebuchet/go/trebuchet_test.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestSumCalibrations(t *testing.T) {
 	result := SumCalibrations("../example_input.txt")
-	fmt.Println(result)
 
 	if result != 142 {
 		t.Errorf("expected 142, got result: %d", result)
+	}
+}
+
+func TestSumCalibrationLetters(t *testing.T) {
+	result := SumCalibrationLetters("../example_input_two.txt")
+
+	if result != 142 {
+		t.Errorf("expected 281, got result: %d", result)
 	}
 }
