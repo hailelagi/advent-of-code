0ad0803d39c5cdb94c9c8f0fad77c6a6cf2038c2
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-10-23 18:12:58 +0100

    cpp file io wip
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- 2023/day-1-trebuchet/cpp/src/main.cpp
+++ 2023/day-1-trebuchet/cpp/src/main.cpp
@@ -1,9 +1,55 @@
+#include <stdexcept>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <exception>
+#include <string>
+#include <vector>
 
-using namespace std;
+/*
+todo(readme): // https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#S-errors
+
+int main() {
+  std::ifstream example_input;
+
+  try {
+    std::ifstream example_input("../../example_input.txt");
+    example_input.exceptions(std::ifstream::failbit);
+  } catch (const std::ios_base::failure &fail) {
+    std::clog << fail.what() << '\n';
+  }
+
+  std::string puzzle;
+
+  while(example_input >> puzzle) {
+    std::cout << puzzle;
+  }
+
+  std::cout << puzzle;
+  std::cout << "answer is:" << '\n';
+
+  return 0;
+}
+*/
 
 int main(void) {
-    cout << "aoc day 1";
+    std::ifstream example_input;
+
+    example_input.open("../../example_input.txt");
+    if (!example_input.is_open()) {
+        throw "could not read puzzle input :(";
+    }
+
+    std::string document;
+    std::vector<std::string> calibration_documents;
+
+    while (example_input >> document) {
+        calibration_documents.push_back(document);
+    }
+
+    example_input.close();
+
+    for (auto i: calibration_documents) std::cout << i << "\n";
+
     return 0;
 }
