f520b51a9d00d20e6ed41e84b83e042079426980
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-12-13 07:16:32 +0100

    2024 day 1 go
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- 2024/day-1-historian-hysteria/go/historian-hysteria.go
+++ 2024/day-1-historian-hysteria/go/historian-hysteria.go
@@ -5,7 +5,11 @@ import (
 	_ "embed"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,14 +18,38 @@ func main() {
 }
 
 func TotalPairDistance(path string) int64 {
-	return 0
+	first, second := parsePuzzleInput(path)
+	var total int64
+
+	sort.Slice(first, func(i, j int) bool { return first[i] < first[j] })
+	sort.Slice(second, func(i, j int) bool { return second[i] < second[j] })
+
+	for i := range first {
+		total += int64(math.Abs(float64(second[i] - first[i])))
+	}
+
+	return total
 }
 
 func SimilarityScore(path string) int64 {
-	return 0
+	first, second := parsePuzzleInput(path)
+	var score int64
+
+	similarity := make(map[int64]int)
+	for _, id := range second {
+		similarity[id]++
+	}
+
+	for _, id := range first {
+		if mult, found := similarity[id]; found {
+			score += int64(mult) * id
+		}
+	}
+
+	return score
 }
 
-func parsePuzzleInput(path string) []string {
+func parsePuzzleInput(path string) ([]int64, []int64) {
 	input, err := os.Open(path)
 
 	if err != nil {
@@ -31,13 +59,16 @@ func parsePuzzleInput(path string) []string {
 
 	defer input.Close()
 
+	var first, second []int64
 	scanner := bufio.NewScanner(input)
-	var locations []string
 
 	for scanner.Scan() {
-		locations = append(locations, scanner.Text())
+		parts := strings.Fields(scanner.Text())
+		f, _ := strconv.ParseInt(parts[0], 10, 64)
+		s, _ := strconv.ParseInt(parts[1], 10, 64)
+		first = append(first, f)
+		second = append(second, s)
 	}
 
-	return locations
-
+	return first, second
 }
