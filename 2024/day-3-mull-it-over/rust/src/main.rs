d01a7345624f9e6438c2f2028f270315afeef0c4
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2025-03-15 18:51:42 +0100

    rust day 3 nom, nom, nom
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- 2024/day-3-mull-it-over/rust/src/main.rs
+++ 2024/day-3-mull-it-over/rust/src/main.rs
@@ -1,38 +1,39 @@
-use std::fs;
 use nom::{
     bytes::complete::tag,
     character::complete::i32,
     sequence::{delimited, separated_pair},
     IResult,
 };
+use std::fs;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Mult {
-  pub x:   i32,
-  pub y:   i32,
+    pub x: i32,
+    pub y: i32,
 }
 
 // https://github.com/rust-bakery/nom/blob/main/doc/choosing_a_combinator.md
 fn main() {
     println!("{:?}", mul_corrupted("../puzzle_input.txt"));
+    println!("{:?}", enabled_mul_corrupted("../puzzle_input.txt"));
 }
 
 fn parse_integer_pair(input: &str) -> IResult<&str, (i32, i32)> {
-    separated_pair(
-        i32,
-        tag(","),
-        i32
-    )(input)
+    separated_pair(i32, tag(","), i32)(input)
 }
 
 fn parse_mul(input: &str) -> IResult<&str, Mult> {
-    let (remaining, (x, y)) = delimited(
-        tag("mul("),
-        parse_integer_pair,
-        tag(")")
-    )(input)?;
+    let (remaining, (x, y)) = delimited(tag("mul("), parse_integer_pair, tag(")"))(input)?;
 
-    Ok((remaining, Mult {x, y}))  
+    Ok((remaining, Mult { x, y }))
+}
+
+fn parse_do(input: &str) -> IResult<&str, &str> {
+    tag("do()")(input)
+}
+
+fn parse_dont(input: &str) -> IResult<&str, &str> {
+    tag("don't()")(input)
 }
 
 fn mul_corrupted(path: &str) -> i32 {
@@ -52,8 +53,40 @@ fn mul_corrupted(path: &str) -> i32 {
     muls.iter().map(|m| m.x * m.y).sum()
 }
 
+fn enabled_mul_corrupted(path: &str) -> i32 {
+    let memory = parse(path);
+    let mut muls = Vec::new();
+    let mut disabled = false;
+    let mut i = 0;
+
+    while i < memory.len() {
+        if let Ok((remaining, _)) = parse_dont(&memory[i..]) {
+            i = memory.len() - remaining.len();
+            disabled = true;
+        } else if let Ok((remaining, _)) = parse_do(&memory[i..]) {
+            i = memory.len() - remaining.len();
+            disabled = false
+        } else if let Ok((remaining, mul)) = parse_mul(&memory[i..]) {
+            i = memory.len() - remaining.len();
+            if disabled {
+                continue;
+            } else {
+                muls.push(mul);
+            }
+        } else {
+            i += 1;
+        }
+    }
+
+    muls.iter().map(|m| m.x * m.y).sum()
+}
+
 fn parse(path: &str) -> String {
-    fs::read_to_string(path).expect("err reading puzzle input!").chars().filter(|c| !c.is_whitespace()).collect()
+    fs::read_to_string(path)
+        .expect("err reading puzzle input!")
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect()
 }
 
 #[cfg(test)]
@@ -61,7 +94,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_safe_reports() {
+    fn test_multiplications() {
         assert_eq!(mul_corrupted("../example_input.txt"), 161)
+    }
+
+    #[test]
+    fn test_enabled_multiplications() {
+        assert_eq!(enabled_mul_corrupted("../example_input_two.txt"), 48)
     }
 }
