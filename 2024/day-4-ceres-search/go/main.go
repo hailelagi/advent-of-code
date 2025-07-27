06579ff8a5e20d9fdf5455632e6b075648d080c4
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2025-07-27 17:04:55 +0100

    2024 day 4
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- 2024/day-4-ceres-search/go/main.go
+++ 2024/day-4-ceres-search/go/main.go
@@ -7,12 +7,42 @@ import (
 	"os"
 )
 
+var directions = [][2]int{
+	{0, 1}, {0, -1}, {1, 0}, {-1, 0},
+	{1, 1}, {-1, -1}, {1, -1}, {-1, 1},
+}
+
 func main() {
 	fmt.Println(xmasCount("../puzzle_input.txt"))
 }
 
 func xmasCount(path string) int {
-	return 0
+	grid := parsePuzzleInput(path)
+	word := "XMAS"
+	rows := len(grid)
+	cols := len(grid[0])
+	count := 0
+
+	for i := range rows {
+		for j := range cols {
+			for _, d := range directions {
+				match := true
+				for k := 0; k < 4; k++ {
+					x := i + k*d[0]
+					y := j + k*d[1]
+					if x < 0 || x >= rows || y < 0 || y >= cols || grid[x][y] != word[k] {
+						match = false
+						break
+					}
+				}
+				if match {
+					count++
+				}
+			}
+		}
+	}
+
+	return count
 }
 
 func parsePuzzleInput(path string) []string {
